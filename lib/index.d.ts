d8e47ea94adf0cefc927a64b8f8250279cae55d2
Chuck <chuck.britt@mosaichcm.com>
2025-07-21 15:33:08 -0600

    Fix v2 endpoints registration in index.ts
    - Added missing imports for tenantsV2 and tenantCredentialsV2 functions
    - This will resolve the 404 errors for /api/v2/tenants endpoints
    - Functions need to be imported in index.ts to register with Azure Functions runtime

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,7 @@
 import { app } from '@azure/functions';
 import './functions/tenants';
+import './functions/tenantsV2';
+import './functions/tenantCredentialsV2';
 import './functions/oauth';
 import './functions/health';
 import './functions/thirdPartyAPIs';
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { app } from '@azure/functions';
 
 // Import all function modules to ensure they register with the app
 import './functions/tenants';
+import './functions/tenantsV2';
+import './functions/tenantCredentialsV2';
 import './functions/oauth';
 import './functions/health';
 import './functions/thirdPartyAPIs';
