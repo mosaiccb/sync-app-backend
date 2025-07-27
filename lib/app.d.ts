cb98c912fcd61815280a2e85bdeb919d077de5fb
Chuck <chuck.britt@mosaichcm.com>
2025-07-26 20:31:10 -0600

    Add PAR Brink Sales Labor data yadda5

--- lib/app.d.ts
+++ lib/app.d.ts
@@ -2,4 +2,6 @@ import './functions/health';
 import './functions/oauth';
 import './functions/tenants';
 import './functions/thirdPartyAPIs';
+import './functions/parBrinkConfigurations';
+import './functions/parBrinkDashboard';
 //# sourceMappingURL=app.d.ts.map
