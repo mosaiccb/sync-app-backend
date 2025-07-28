eb96419240af48e4d138b8a886da730f4831a777
Chuck <chuck.britt@mosaichcm.com>
2025-07-28 15:37:08 -0600

    🚀 Backend deployment: Restaurant Operations Dashboard API updates

--- lib/functions/parBrinkEnhanced.d.ts
+++ lib/functions/parBrinkEnhanced.d.ts
@@ -1,6 +1,11 @@
 import { HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 export interface ParBrinkEmployee {
     EmployeeId: string;
+    FirstName: string;
+    LastName: string;
+    Status: string;
+    Position: string;
+    HourlyRate: number;
 }
 export interface ParBrinkShift {
     ShiftId: string;
