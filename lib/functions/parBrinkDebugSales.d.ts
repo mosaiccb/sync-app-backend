664f6bc7f82367351c883688375167b13ef0d30b
Chuck <chuck.britt@mosaichcm.com>
2025-07-28 11:52:07 -0600

    🚀 Backend deployment: Restaurant Operations Dashboard API updates

---
+++ lib/functions/parBrinkDebugSales.d.ts
@@ -0,0 +1,3 @@
+import { HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+export declare function parBrinkDebugSales(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit>;
+//# sourceMappingURL=parBrinkDebugSales.d.ts.map
